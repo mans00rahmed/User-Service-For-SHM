cd6678e88198de6cac5d239da10db3a0e9740466
mans00rahmed <mansoorahmedy2k@gmail.com>
2026-02-07 01:36:24 +0000

    packages commit

---
+++ src/main/java/com/sigma/smarthome/maintenance_service/Controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.sigma.smarthome.maintenance_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Maintenance Service is up and running 🚀";
+    }
+}
