70d88af94f3cd5d84fa8018ad42be5815dcceb28
mans00rahmed <mansoorahmedy2k@gmail.com>
2026-02-11 14:29:10 +0000

    interservice communication checked

--- src/main/java/com/sigma/smarthome/maintenance_service/Controller/UserController.java
+++ src/main/java/com/sigma/smarthome/maintenance_service/Controller/UserController.java
@@ -2,9 +2,26 @@ package com.sigma.smarthome.maintenance_service.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
+
+
+    private final RestTemplate restTemplate;
+
+    public UserController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/users/property-test")
+    public String callPropertyService() {
+
+        return restTemplate.getForObject(
+                "http://property-service/properties/test",
+                String.class
+        );
+    }
 
     @GetMapping("/users/test")
     public String testUserService() {
