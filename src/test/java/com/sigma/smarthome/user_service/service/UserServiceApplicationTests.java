3f637be170d149859b3a341e8218d82a534b30c6
Taha Aflouk <tahaaflouk94@gmail.com>
2026-02-12 15:19:24 +0000

    SMPM-24: remove maintenance test and setup UserService SpringBootTest

---
+++ src/test/java/com/sigma/smarthome/user_service/service/UserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sigma.smarthome.user_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sigma.smarthome.user_service.UserServiceApplication;
+
+@SpringBootTest(classes = UserServiceApplication.class)
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
